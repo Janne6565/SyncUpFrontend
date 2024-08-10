c5c1dde695a8d290005f86020ca5e2b44e5cd213
janne6565 <jabbekeipert@gmail.com>
2024-08-10 14:44:24 +0200

    fix linting problems

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,7 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home/HomePage";
-import NotFoundPage from "./pages/errors/404/NotFoundPage";
 import Layout from "./components/util/Layout";
-import { ThemeProvider } from "@mui/material";
+import NotFoundPage from "./pages/errors/404/NotFoundPage";
+import HomePage from "./pages/home/HomePage";
 import MapPage from "./pages/maps/MapPage";
 import SearchPage from "./pages/search/SearchPage";
 
@@ -16,8 +14,8 @@ function RouteManagerComponent(props: Props) {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/maps" element={<MapPage/>}/>
-        <Route path="/search" element={<SearchPage/>}/>
+        <Route path="/maps" element={<MapPage />} />
+        <Route path="/search" element={<SearchPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { ThemeProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import RouteManagerComponent from "./Routes.tsx";
-import { ThemeProvider } from "@mui/material";
 import theme from "./theme/Theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
